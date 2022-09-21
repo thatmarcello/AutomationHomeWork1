23a27a38b3b0b752adfe85fa8ec39a394725f9b0
Mark Iarantsev <markyarantsev@yahoo.com>
2022-09-21 23:24:23 +0300

    added tests in testing branch

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnRemained () {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    // купил на 1000 ответ 0
+    @Test
+    public void shouldNotAskToIncreaseTheAmountIfDivisibleByBoundary  () {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    // купил на 1001 ответ 999
+    @Test
+    public void shouldReturnedRemainedOverBoundary () {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void shouldReturnRemainedInMidOfBoundary () {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
